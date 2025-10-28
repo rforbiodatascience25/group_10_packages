1dad11a9ec23d390bf36dfd0f2eb997889c3f1f8
molly-hongminh <molly.hongminh@gmail.com>
2025-10-28 11:22:28 +0100

    Edit description (transc/transl)

--- R/translate.R
+++ R/translate.R
@@ -4,9 +4,3 @@
 #'
 #' @returns A character string representing the translated amino acid sequence (one-letter code).
 #' @export
-#'
-#' @examples
-translate <- function(codons){
-  protein <- paste0(codon_table[codons], collapse = "")
-  return(protein)
-}
