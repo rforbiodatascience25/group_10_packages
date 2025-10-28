dc7936a06aabcc678cc9fa043e54a2bf0fb9480f
molly-hongminh <molly.hongminh@gmail.com>
2025-10-28 16:12:43 +0100

    Edit Transcription

--- R/transcription.R
+++ R/transcription.R
@@ -1,9 +1,13 @@
 #' Transcription
 #'
+#' Convert a DNA sequence to RNA by replacing thymine (T) with uracil (U).
+#'
 #' @param dna_seq dna sequence
 #'
 #' @returns Substitutes T with U in a given dna seq to generate rna seq
 #' @export
+#' @examples
+#' transcription("ATGCTTAGGCTA")
 
 transcription <- function(dna_seq){
   rna_seq <- gsub("T", "U", dna_seq)
