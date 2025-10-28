af81015fbab4e221a7b442608353442888d9aeed
molly-hongminh <molly.hongminh@gmail.com>
2025-10-28 11:36:04 +0100

    Change skeleton

--- R/transcription.R
+++ R/transcription.R
@@ -1,10 +1,8 @@
 #' Transcription
 #'
-#' Substitute T with U
-#'
 #' @param dna_seq dna sequence
 #'
-#' @returns substitutes T with U in a given dna seq to generate rna seq
+#' @returns Substitutes T with U in a given dna seq to generate rna seq
 #' @export
 transcription <- function(dna_seq){
   rna_seq <- gsub("T", "U", dna_seq)
